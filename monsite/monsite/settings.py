b613574d8411479fb50103265aea1f8d382d8988
Quentin Rollet <rollet.qunetin@orange.fr>
2022-10-24 08:56:05 +0200

    View change in settings

--- monsite/monsite/settings.py
+++ monsite/monsite/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://quentin-rollet-portfolio.herokuapp.com/']
+CSRF_TRUSTED_ORIGINS = ['https://quentin-rollet-portfolio.herokuapp.com']
 
 
 # Application definition
